af4468dd552a3aad597091911b1357fe8bba49f6
Thanhtinh06 <nguyenthithanhtinh06@gmail.com>
2023-02-22 11:16:13 +0700

    -fix add product

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -30,7 +30,9 @@ getEle("cartShop").addEventListener("onclick", function () {
   getEle("cartShop").style.visibility = "hidden";
 });
 
-
+getEle("closeNoti").addEventListener("click", function () {
+  getEle("notiProduct").style.visibility = "hidden";
+});
 
 function getListProduct() {
   callApi
@@ -43,30 +45,29 @@ function getListProduct() {
     });
 }
 
-
-
 function addToCart(id) {
   getEle(`btn${id}`).style.display = "flex";
   getEle(`add${id}`).style.display = "none";
-  getEle(`btn${id}`).setAttribute('hasCart','true');
 
   callApi
     .getDetailProduct(id)
     .then((result) => {
       let item = cartList.find((item) => item.product.id === result.data.id);
-      if(item == undefined){
+      if (item == undefined) {
         const cartItem = new CartItem(result.data);
         cartItem.id = result.data.id;
         cartList.push(cartItem);
         getQuery(`.quality${id}`).innerHTML = cartItem.quality;
         setLocalStage();
         getLocalStage();
+      } else {
+        let amount = getAmount(id)
+        getQuery(`.quality${id}`).innerHTML = amount;
       }
     })
     .catch(function (error) {
       console.log(error);
     });
-
 }
 
 function changeQuality(id, isPlus) {
@@ -74,54 +75,91 @@ function changeQuality(id, isPlus) {
   if (item) {
     let qualityElement = getQuery(`.quality${id}`);
     let quality = parseInt(qualityElement.innerHTML);
-    if (quality > 0) {
+    if (quality > 0 && quality < 11) {
       if (isPlus) {
         quality += 1;
       } else {
         quality -= 1;
       }
       item.quality = quality;
-    } else if (quality == 0) {
+    }
+    if (quality >= 10) {
+      getEle("notiProduct").style.visibility = "visible";
+    }
+    qualityElement.innerHTML = item.quality;
+    if (quality < 1) {
       deleteItem(id);
       getEle(`btn${id}`).style.display = "none";
       getEle(`add${id}`).style.display = "block";
     }
-    qualityElement.innerHTML = item.quality;
     setLocalStage();
     getLocalStage();
   }
+}
+
+function getAmount(id) {
+  let hasItem = cartList.find((item) => item.product.id == id);
+  return hasItem.quality
 }
 
 function clearAllCart() {
   cartList = [];
   setLocalStage();
   getLocalStage();
+  getListProduct();
 }
 
-function deleteItem(cartItemID){
+function deleteItem(cartItemID) {
   let index = cartList.findIndex((item) => item.id == cartItemID);
-  if(index !== -1) {
-    cartList.splice(index,1);
+  if (index !== -1) {
+    cartList.splice(index, 1);
     setLocalStage();
     getLocalStage();
+    getListProduct();
   }
+}
+
+function getTotalPayment() {
+  totalPayment = cartList.reduce((totalPayment, cartItem) => {
+    return (totalPayment +=
+      parseFloat(cartItem.quality) * parseFloat(cartItem.product.price));
+  }, 0);
+  getEle("totalCart").innerHTML = `Total: ${totalPayment}`;
+}
+
+function getTotalAmount() {
+  totalAmount = cartList.reduce((totalAmount, cartItem) => {
+    return (totalAmount += cartItem.quality);
+  }, 0);
+  getEle("amount-product").innerHTML = totalAmount;
 }
 
 function setLocalStage() {
   localStorage.setItem("CartList", JSON.stringify(cartList));
 }
+
 function getLocalStage() {
   let dataString = localStorage.getItem("CartList");
   cartList = JSON.parse(dataString);
   renderCartList(cartList);
-  totalPayment = cartList.reduce((totalPayment,cartItem) => {
-    return totalPayment += parseFloat(cartItem.quality) * parseFloat(cartItem.product.price)
-  },0)
-  totalAmount = cartList.reduce((totalAmount,cartItem)=>{
-    return totalAmount += cartItem.quality
-  },0)
-  getEle("amount-product").innerHTML = totalAmount;
-  getEle("totalCart").innerHTML = `Total: ${totalPayment}`;
+  getTotalAmount();
+  getTotalPayment();
 }
+
+// const filterProduct = (type="Android") => {
+//   let arrFilterProduct = [];
+//   callApi 
+//   .fetchListData()
+//   .then((result)=>{
+    
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   })
+//   return arrFilterProduct
+// } 
+
+
+
 
 getListProduct();
